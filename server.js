99b3e1e0ad6ed8dcf43e6b8817665ee208d105f8
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-27 21:25:50 +0100

    Add concerts and seats routes

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const cors = require('cors');
-const uuid = require('uuid').v4;
 const db = require('./db.js');
 
 const app = express();
 
 //import routes
 const testimonialRoutes = require('./routes/testimonials.routes.js');
+const concertsRoutes = require('./routes/concerts.routes.js');
+const seatsRoutes = require('./routes/seats.routes.js');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
 app.use('/api/', testimonialRoutes);
+app.use('/api/', concertsRoutes);
+app.use('/api/', seatsRoutes);
 
 app.use((req, res) => {
     res.status(404).send('404 not found...');
