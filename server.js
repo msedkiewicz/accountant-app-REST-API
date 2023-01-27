e49ae99081354120fda868235436bf35476ba6ab
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-27 20:04:44 +0100

    Add basic database

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+const db = [
+    { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+    { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+    { id: 3, author: 'Jane Doe', text: 'Our company profited from cooperation.' }
+];
+
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
 });
