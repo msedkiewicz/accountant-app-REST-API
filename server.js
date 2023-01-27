2378d55f9fd5a91d334c31ab19c0fccb8ca2506b
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-01-27 20:03:03 +0100

    Add basic configuration

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const uuid = require('uuid').v4;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+});
